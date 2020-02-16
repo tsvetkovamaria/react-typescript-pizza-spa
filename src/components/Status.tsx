0bce0fdf4cef060ea206efa2e7e4e86794e0fa73
Maria Tsvetkova <tsvetkovamariia@gmail.com>
2020-02-16 13:47:57 +0600

    mock status data

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,9 +1,58 @@
 import React from "react";
 
-const Status = () => {
+interface Order {
+    id: string
+    address: string
+    time: string
+    accepted: boolean
+    transit: boolean
+    completed: boolean
+    canceled: boolean
+}
+
+interface Props {
+    orders: Order[]
+}
+
+const Status = ({ orders }: Props) => {
     return (
         <div>
-            <h2>Status</h2>
+            <h1>Order management</h1>
+            <p>Manage your orders here and get an overview of status</p>
+            <div>
+                <p>Total items in inventory</p>
+                <div>
+                    <span>1,508</span>
+                    <span> items</span>
+                </div>
+                <div>
+                    <div>Green, blue, red line</div>
+                    <div>Completed 326</div>
+                    <div>Pending 581</div>
+                    <div>Cancelled 129</div>
+                </div>
+                <hr/>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Item Id</th>
+                            <th>Address</th>
+                            <th>Order time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        orders.map(order =>
+                            <tr>
+                                <td>{order.id}</td>
+                                <td>{order.address}</td>
+                                <td>{order.time}</td>
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
