eac89272ac63b6d53a8fc89e5ba9c1cbb49e097c
Maria Tsvetkova <tsvetkovamariia@gmail.com>
2020-02-17 02:19:54 +0600

    create order page: add basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -12,30 +12,38 @@ import OrderCreatePage from "./pages/OrderCreate.page";
 function App() {
     return (
         <Router>
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/">Dashboard</Link>
-                    </li>
-                    <li>
-                        <Link to="/new">New order</Link>
-                    </li>
-                    <li>
-                        <Link to="/status">Status</Link>
-                    </li>
-                </ul>
-                <hr />
-                <Switch>
-                    <Route exact path="/">
-                        <DashboardPage />
-                    </Route>
-                    <Route path="/new">
-                        <OrderCreatePage />
-                    </Route>
-                    <Route path="/status">
-                        <StatusPage />
-                    </Route>
-                </Switch>
+            <div className="bg-blue-900 text-white h-screen">
+                <div className="container mx-auto px-8">
+                    <header className="py-4">
+                        <Link className="text-2xl" to="/">Pizza Manager</Link>
+                    </header>
+                    <div className="flex justify-between">
+                        <ul className="w-1/6">
+                            <li>
+                                <Link to="/">Dashboard</Link>
+                            </li>
+                            <li>
+                                <Link to="/new">New order</Link>
+                            </li>
+                            <li>
+                                <Link to="/status">Status</Link>
+                            </li>
+                        </ul>
+                        <div className="text-black bg-white rounded-lg w-5/6 px-12 py-12">
+                            <Switch>
+                                <Route exact path="/">
+                                    <DashboardPage />
+                                </Route>
+                                <Route path="/new">
+                                    <OrderCreatePage />
+                                </Route>
+                                <Route path="/status">
+                                    <StatusPage />
+                                </Route>
+                            </Switch>
+                        </div>
+                    </div>
+                </div>
             </div>
         </Router>
     );
--- src/components/NewOrder.tsx
+++ src/components/NewOrder.tsx
@@ -120,16 +120,17 @@ class NewOrder extends React.Component<Props, State>{
     render() {
         return (
             <div>
-                <h1>Pizza order</h1>
-                <h3>Basic information</h3>
+                <h1 className="uppercase text-3xl mb-6">Pizza order</h1>
+                <h3 className="text-lg font-bold mb-3">Basic information</h3>
                 <hr/>
-                <div>
+                <div className="grid grid-cols-2 gap-10 py-4 mb-8">
                     <div>
                         <label>
-                            <span>Name</span>
+                            <span className="block text-sm mb-2">Name</span>
                             <input
                                 type="text"
                                 name="name"
+                                className="block w-full border p-2"
                                 value={this.state.client.name}
                                 onChange={this.onTextChange}
                             />
@@ -137,10 +138,11 @@ class NewOrder extends React.Component<Props, State>{
                     </div>
                     <div>
                         <label>
-                            <span>E-mail Address</span>
+                            <span className="block text-sm mb-2">E-mail Address</span>
                             <input
                                 type="text"
                                 name="email"
+                                className="block w-full border p-2"
                                 value={this.state.client.email}
                                 onChange={this.onTextChange}
                             />
@@ -148,10 +150,11 @@ class NewOrder extends React.Component<Props, State>{
                     </div>
                     <div>
                         <label>
-                            <span>Address</span>
+                            <span className="block text-sm mb-2">Address</span>
                             <input
                                 type="text"
                                 name="address"
+                                className="block w-full border p-2"
                                 value={this.state.client.address}
                                 onChange={this.onTextChange}
                             />
@@ -159,10 +162,11 @@ class NewOrder extends React.Component<Props, State>{
                     </div>
                     <div>
                         <label>
-                            <span>Contact number</span>
+                            <span className="block text-sm mb-2">Contact number</span>
                             <input
                                 type="tel"
                                 name="phone"
+                                className="block w-full border p-2"
                                 value={this.state.client.phone}
                                 onChange={this.onTextChange}
                             />
@@ -170,20 +174,32 @@ class NewOrder extends React.Component<Props, State>{
                     </div>
                 </div>
                 <div>
-                    <h3>Choose your pizza</h3>
-                    <button onClick={this.addItem}>Add pizza</button>
+                    <div className="flex justify-between items-center mb-2">
+                        <h3 className="font-bold text-lg">Choose your pizza</h3>
+                        <button
+                            className="text-xs border border-2 text-green-500 border-green-300 px-4 py-2"
+                            onClick={this.addItem}
+                        >Add pizza</button>
+                    </div>
+
                     {/* Pizza form*/}
                     { this.state.order.map((item,itemIndex) => (
-                        <div key={itemIndex}>
-                            <div>
-                                <h4>Pizza {itemIndex + 1}</h4>
-                                <button>Remove Pizza</button>
+                        <div key={itemIndex} className="mb-6">
+                            <div className="bg-gray-800 text-white p-3 flex justify-between items-center">
+                                <h4 className="uppercase font-bold text-lg">Pizza {itemIndex + 1}</h4>
+                                <button
+                                    className="text-xs border border-2 text-red-500 border-red-500 px-4 py-2">
+                                    Remove Pizza
+                                </button>
                             </div>
-                            <div>
-                                <h4>Choose size</h4>
+                            <div className="p-4">
+                                <h4 className="font-bold text-lg mb-4">Choose size</h4>
                                 {
                                     this.props.sizes.map((size, i) => (
-                                        <label key={size.name + i}>
+                                        <label
+                                            key={size.name + i}
+                                            className={'px-6 py-3 inline-block border border-blue mr-2 ' + (item.size.id === size.id? 'bg-blue-400 text-white': '')}
+                                        >
                                             <input
                                                 type="radio"
                                                 name={'size' + itemIndex}
@@ -191,17 +207,24 @@ class NewOrder extends React.Component<Props, State>{
                                                 data-item={itemIndex}
                                                 onChange={this.onSizeChange}
                                                 value={ i }
+                                                className='hidden'
                                             />
-                                            { size.name }
+                                            <span>
+                                                { size.name }
+                                            </span>
                                         </label>
                                     ))
                                 }
                             </div>
-                            <div>
-                                <h4>Pick your toppings</h4>
+                            <div className="p-4">
+                                <h4 className="font-bold text-lg mb-4">Pick your toppings</h4>
+                                <div className="grid grid-cols-4 gap-3">
                                 {
                                     this.props.toppings.map((topping, i) => (
-                                        <label key={topping.name + i}>
+                                        <label
+                                            key={topping.name + i}
+                                            className={'px-6 py-3 border border-blue mr-2 ' + (item.toppings[topping.id] !== undefined? 'bg-blue-500 text-white': '')}
+                                        >
                                             <input
                                                 type="checkbox"
                                                 name={'topping' + itemIndex}
@@ -209,43 +232,52 @@ class NewOrder extends React.Component<Props, State>{
                                                 onChange={this.onToppingChange}
                                                 checked={ item.toppings[topping.id] !== undefined }
                                                 value={ i }
+                                                className="hidden"
                                             />
                                             {topping.name}
                                         </label>
                                     ))
                                 }
+                                </div>
                             </div>
                         </div>
                     ))}
                 </div>
                 {/*Summary*/}
-                <div>
-                    <h2>Summary</h2>
-                    <hr/>
+                <div className="mb-10">
+                    <h2 className="text-lg font-bold mb-3">Summary</h2>
+                    <hr className="mb-4" />
                     {
                         this.state.order.map((item, i) => (
-                            <div key={i}>
-                                <div>
-                                    <b>{item.size.name} Pizza {i + 1}</b>
-                                    <span>..........</span>
-                                    <b>$ {this.getPrice(item)}</b>
+                            <div key={i} className="text-sm">
+                                <div className="flex justify-between mb-2 mt-4">
+                                    <b className="mr-8 whitespace-no-wrap">{item.size.name} Pizza {i + 1}</b>
+                                    <span className="w-full border-b"></span>
+                                    <b className="ml-8 whitespace-no-wrap">$ {this.getPrice(item)}</b>
                                 </div>
                                 {
                                     Object.values(item.toppings).map(topping => (
-                                        <div key={topping.name}>
-                                            <b>{topping.name}</b>
-                                            <b>{topping.price}</b>
+                                        <div key={topping.name} className="pl-10 flex justify-between">
+                                            <span>{topping.name}</span>
+                                            <span>{topping.price}</span>
                                         </div>
                                     ))
                                 }
                             </div>
                         ))
                     }
-                    <hr/>
-                    <span>TOTAL</span>
-                    <span>$ {this.getTotal()}</span>
+                    <hr className="my-4"/>
+                    <div className="flex justify-between text-2xl bold">
+                        <span>TOTAL</span>
+                        <span>$ {this.getTotal()}</span>
+                    </div>
                 </div>
-                <button onClick={this.onSubmit}>Place Order</button>
+                <div className="text-right">
+                    <button
+                        className="inline-block text-xs border border-2 text-teal-500 border-teal-300 px-4 py-2"
+                        onClick={this.onSubmit}
+                    >Place Order</button>
+                </div>
             </div>
         );
     }
